2782689f250e7f32c656ab20533fae1e8e7529ce
julianlizarraga15 <julian.lizarraga.15@gmail.com>
2022-01-06 15:51:46 -0300

    Segundo

--- .buildozer/android/platform/build-armeabi-v7a/dists/myapp__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/julian/Documents/cuotadora/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/julian/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.c \
-  /home/julian/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h
-
-/home/julian/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h:
