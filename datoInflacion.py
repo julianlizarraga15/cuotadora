138f5c7f68596a897da4fac0116311487f4cb732
Julián Ignacio Lizarraga <jlizarraga@practia.global>
2022-02-05 16:08:04 -0300

    Popup de error al cargar datos

--- datoInflacion.py
+++ datoInflacion.py
@@ -1,6 +1,23 @@
 import requests
+from kivy.uix.popup import Popup
+import re
 
 def get_dato_inflacion():
-
     r = requests.get('https://cuotadora-default-rtdb.firebaseio.com/.json')
     return r.json()
+
+def chequear_input_inflacion(inflacion):
+
+    if inflacion.isnumeric():
+        return True
+    else:
+        busqueda = re.search('\d+(\.|\,)\d+', inflacion)
+        if busqueda is not None:
+            return True
+        else:
+            popup = ErrorPopup()
+            popup.open()
+            return False
+
+class ErrorPopup(Popup):
+    pass
--- main.py
+++ main.py
@@ -7,50 +7,51 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
-from datoInflacion import get_dato_inflacion
+from kivy.uix.popup import Popup
+from datoInflacion import get_dato_inflacion, chequear_input_inflacion
+from datoInflacion import ErrorPopup
 import math
 
 __version__ = '0.1'
 
 class MyGridLayout(GridLayout):
 
-    # input_monto = ObjectProperty(None)
-    # input_n_cuotas = ObjectProperty(None)
-    # input_inflacion_anual_estimada = ObjectProperty(None)
-    # label_npv = ObjectProperty(None)
-    # label_descuento = ObjectProperty(None)
-    # label_ahorro = ObjectProperty(None)
-
     def calcular(self):
-        monto_total = float(self.input_monto.text)
-        n_cuotas = int(self.input_n_cuotas.text)
-        inflacion_estimada = float(self.input_inflacion_anual_estimada.text)
 
-        # Calcular valor presente neto
-        inflacion_estimada = inflacion_estimada / 100
-        inflacion_mensual = (1 + inflacion_estimada)**(1/12) - 1
-        monto_mensual = monto_total / n_cuotas
-        vpn = 0
-        for cuota in range(n_cuotas):
-            vpn += monto_mensual / ((1+inflacion_mensual)**cuota)
-        vpn = math.floor(vpn)
+        try:
+            monto_total = float(self.input_monto.text)
+            n_cuotas = int(self.input_n_cuotas.text)
+            inflacion_estimada = self.input_inflacion_anual_estimada.text
 
-        # Calcular porcentaje de descuento
-        porcentaje_descuento = 100 - vpn*100/monto_total
+            inflacion_ok = chequear_input_inflacion(inflacion_estimada)
 
-        # Calcular monto ahorrado
-        monto_ahorro = monto_total - vpn
+            if isinstance(monto_total, float) and isinstance(n_cuotas, int) and inflacion_ok:
+                # Convertir a float la inflacion_estimada
+                inflacion_estimada = float(inflacion_estimada.replace(',', '.'))
+                # Calcular valor presente neto
+                inflacion_estimada = inflacion_estimada / 100
+                inflacion_mensual = (1 + inflacion_estimada)**(1/12) - 1
+                monto_mensual = monto_total / n_cuotas
+                vpn = 0
+                for cuota in range(n_cuotas):
+                    vpn += monto_mensual / ((1+inflacion_mensual)**cuota)
+                vpn = math.floor(vpn)
 
-        # Print to the screen
-        self.ids.label_npv.text = '$' + str(vpn)
-        self.ids.label_descuento.text = str(round(porcentaje_descuento, 2)) + '%'
-        self.ids.label_ahorro.text = '$' + str(math.floor(monto_ahorro))
+                # Calcular porcentaje de descuento
+                porcentaje_descuento = 100 - vpn*100/monto_total
 
-        # Clear the input boxes
-        # self.monto_total.text = ''
-        # self.n_cuotas.text = ''
-        # self.inflacion_estimada.text = ''
+                # Calcular monto ahorrado
+                monto_ahorro = monto_total - vpn
 
+                # Print to the screen
+                self.ids.label_npv.text = '$' + str(vpn)
+                self.ids.label_descuento.text = str(round(porcentaje_descuento, 2)) + '%'
+                self.ids.label_ahorro.text = '$' + str(math.floor(monto_ahorro))
+
+        except Exception as e:
+            popup = ErrorPopup()
+            popup.open()
+    
 
 class Cuotadora(App):
     def build(self):
